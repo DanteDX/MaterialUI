3d2dd4fccadd09395945cee6db67e3e5fd157a97
dantedxwolf <teacher.martin@gmail.com>
2021-01-01 11:43:32 +0600

    #1 finished

---
+++ src/CustomStyles.js
@@ -0,0 +1,14 @@
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    typoStyles:{
+        textDecoration:"underline",
+        fontWeight:"bold"
+        
+    },
+    buttonStyles:{
+        borderRadius:"20px"
+    }
+})
+
+export default useStyles;
