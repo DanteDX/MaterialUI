bd321e15be6427f5b458500ca0e0ed3a5f694bea
dantedxwolf <teacher.martin@gmail.com>
2021-01-01 08:23:24 +0600

    initial study

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from 'react';
+import {Button} from "@material-ui/core";
+const App = () =>{
+  return(
+    <div>
+      <Button variant="contained" color="primary" disableElevation={true}>primary contained  variant</Button>
+      <Button variant="contained" color="secondary" disableFocusRipple={true}>secondary contained  variant</Button>
+      <Button variant="outlined" color="secondary">secondary outlined variant</Button>
+      <Button variant="outlined" color="primary" disabled={true}>primary outlined variant</Button>
     </div>
-  );
+  )
 }
 
 export default App;
