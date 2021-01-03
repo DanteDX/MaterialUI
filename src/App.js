4c9b2bb796cee91286d1fa72fdc9ace5a02e96c2
dantedxwolf <teacher.martin@gmail.com>
2021-01-03 08:38:36 +0600

    Drawer Navigation added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Drawer, Button,ListItem,ListItemIcon,ListItemText,Divider } from "@material-ui/core";
+import {createMuiTheme,ThemeProvider} from "@material-ui/core/styles";
+import FacebookIcon from "@material-ui/icons/Facebook";
 
 const App = () => {
-    return (
-        <div>
-            <p>is this still working </p>
-        </div>
-    )
-}
+  const darkTheme = createMuiTheme({
+    palette: {
+        type: "dark",
+    },
+    });
+  const [status, setStatus] = React.useState(false);
+  return (
+    <ThemeProvider theme={darkTheme}>
+    <div>
+      
+      <Drawer open={status} anchor="right" variant="persistent" style={{width:"200px"}}>
+        <FacebookIcon style={{width:"400px"}}/>
+        <Divider/>
+        <Button variant="contained" color="secondary" onClick={e => setStatus(!status)}>Close Drawer</Button>
+        <Divider />
+        <ListItem button>
+            <ListItemIcon>{<FacebookIcon/>}</ListItemIcon>
+            <ListItemText color="primary">Hello World</ListItemText>
+        </ListItem>
+      </Drawer>
+      <Button variant="outlined" color="primary" onClick={(e) => setStatus(!status)}>Open Drawer</Button>
+    </div>
+    </ThemeProvider>
+  );
+};
 
 export default App;
